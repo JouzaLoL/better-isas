9c48951a83b39356bf952d6b44707ec8705f6a86
Josef Vacek <vacekj@outlook.com>
2018-06-12 18:58:15 +0200

    fix login

--- isasLib.js
+++ isasLib.js
@@ -8,29 +8,6 @@ const request = requestProm.defaults({
     baseUrl: "http://isas.gytool.cz/isas/"
 });
 
-
-/**
- * Get znamky
- *
- * @param {*} cookieJar
- * @returns
- */
-async function getZnamky(cookieJar) {
-    const resBuffer = await request("/prubezna-klasifikace.php", {
-        jar: cookieJar,
-        encoding: null
-    });
-
-    return parsePrubeznaKlasifikace(decodeWin1250(resBuffer));
-}
-
-
-/**
- * Log into iSAS
- *
- * @param {*} { username, password }
- * @returns
- */
 async function logIn({ username, password }) {
     try {
         /* Create a cookie jar on every session to prevent cookie leaks */
@@ -54,6 +31,42 @@ async function logIn({ username, password }) {
 }
 
 /**
+ * Log into iSAS
+ *
+ * @returns
+ */
+async function getZnamky(cookieJar) {
+    try {
+        const resBuffer = await request("/prubezna-klasifikace.php", {
+            jar: cookieJar,
+            encoding: null
+        });
+
+        return parsePrubeznaKlasifikace(decodeWin1250(resBuffer));
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getDetail(url, cookieJar) {
+    try {
+        const html = await request(url, {
+            jar: cookieJar
+        });
+        return parseDetail(html);
+    } catch (error) {
+        throw error;
+    }
+}
+
+function parseDetail(html) {
+    const $ = cheerio.load(html);
+
+    const table = $(".isas-histogram");
+    return table;
+}
+
+/**
  * Parse prubezna-klasifikace
  * 
  * @param {any} html prubezna-klasifikace html
@@ -64,9 +77,10 @@ function parsePrubeznaKlasifikace(html) {
     const trs = $("#isas-obsah > table > tbody tr");
     const markTrs = trs.toArray().slice(1);
 
-    function parseTr(markTr) {
+    function parseMarkTr(markTr) {
         return {
             datum: markTr.firstChild.firstChild.firstChild.data,
+            link: markTr.firstChild.firstChild.attribs.href,
             predmet: markTr.children[1].firstChild.data,
             znamka: markTr.children[2].firstChild.data,
             vaha: parseInt(markTr.children[3].firstChild.data.slice(1)),
@@ -76,7 +90,63 @@ function parsePrubeznaKlasifikace(html) {
     }
 
     return markTrs
-        .map(parseTr);
+        .map(parseMarkTr);
 }
 
-module.exports = { getZnamky, logIn };
+/**
+ *    Filter out letters (A, N)
+ *
+ * @param {*} marks
+ * @returns
+ */
+function filterOutLetters(marks) {
+    return marks
+        .filter((znamka) => !isNaN(znamka.znamka))
+        .map((z) => {
+            return Object.assign(z, { znamka: parseInt(z.znamka) });
+        });
+}
+
+function prumery(z) {
+    const znamky = filterOutLetters(z);
+    const predmety = Array.from(new Set(znamky.map((znamka) => znamka.predmet)));
+    const result = predmety.map((predmet) => {
+        const znamkyZPredmetu = znamky
+            .filter((znamka) => znamka.predmet === predmet);
+        const vazenySoucet = znamkyZPredmetu
+            .reduce((soucet, curr) => {
+                return soucet + curr.znamka * curr.vaha;
+            }, 0);
+
+        const soucetVah = znamkyZPredmetu
+            .reduce((soucet, curr) => {
+                return soucet + curr.vaha;
+            }, 0);
+
+        const vazenyPrumer = vazenySoucet / soucetVah;
+        return {
+            vazenyPrumer: Number(vazenyPrumer.toFixed(2)),
+            predmet
+        };
+    });
+
+    return result;
+}
+
+function isVyznamenani(znamky) {
+    const averages = prumery(znamky).map((vazenyPrumer) => {
+        return { ...vazenyPrumer, vyslednaZnamka: Math.round(vazenyPrumer.vazenyPrumer) };
+    }).sort((a, b) => a.vazenyPrumer - b.vazenyPrumer);
+    const numberOf2s = averages.filter((avg) => avg.vyslednaZnamka === 2).length;
+    const numberOf1s = averages.filter((avg) => avg.vyslednaZnamka === 2).length;
+    return averages.every((avg) => avg.vyslednaZnamka <= 2) && numberOf1s > numberOf2s;
+}
+
+const interpolate = require("color-interpolate");
+
+/* Represent a prumer with color ranging from white for 1 to red for 5 */
+const prumerToRgb = (prumer) => {
+    return interpolate(["white", "red"])((prumer - 1) / 4);
+};
+
+module.exports = { getZnamky, prumerToRgb, isVyznamenani, prumery, getDetail, logIn };
