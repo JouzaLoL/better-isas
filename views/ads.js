293ad3edd62576c672b245ffe8058cc1aeb6f59d
Josef Vacek <vacekj@outlook.com>
2018-09-28 11:50:18 +0200

    fix: ads

--- views/ads.js
+++ views/ads.js
@@ -1,13 +1,7 @@
 module.exports = () => `<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-1168224081669943",
-        enable_page_level_ads: true
-    });
-</script>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Better-iSAS -->
-<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-1168224081669943" data-ad-slot="4718975494" data-ad-format="auto"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-1168224081669943",
+          enable_page_level_ads: true
+     });
 </script>`;
--- views/base.js
+++ views/base.js
@@ -28,7 +28,7 @@ module.exports = (content = "", head = "") => `<!DOCTYPE html>
 
     <!-- Head interpolation -->
     ${head}
-    ${require("./ads")}
+    ${require("./ads")()}
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=WGLpp0AGmP">
